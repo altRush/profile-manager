ac232bebc965fb04ccb95389c67c503d28e2db55
Rush WP <rushing.wp@gmail.com>
2018-09-11 04:59:51 +0700

    Update loading screen and favicon

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return !robots.length ?
-      <h1 className='tc'>Loading</h1> :
+    <h1 className='tc pt6 fs'>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>Robo Searcher</h1>
