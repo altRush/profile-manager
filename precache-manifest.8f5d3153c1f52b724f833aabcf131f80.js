aaa4b6d33714e26f62f7bdec520571ac648c0d7b
Rush WP <rushing.wp@gmail.com>
2018-11-09 19:07:24 +0700

    Updates

---
+++ precache-manifest.8f5d3153c1f52b724f833aabcf131f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8588e6cb7662b97b058c",
+    "url": "/profile-manager/static/js/runtime~main.8588e6cb.js"
+  },
+  {
+    "revision": "1dda2af9f5c83208bf50",
+    "url": "/profile-manager/static/js/main.1dda2af9.chunk.js"
+  },
+  {
+    "revision": "6cd3b2aaac2620361ece",
+    "url": "/profile-manager/static/js/1.6cd3b2aa.chunk.js"
+  },
+  {
+    "revision": "1dda2af9f5c83208bf50",
+    "url": "/profile-manager/static/css/main.79bb6643.chunk.css"
+  },
+  {
+    "revision": "6cd3b2aaac2620361ece",
+    "url": "/profile-manager/static/css/1.2925258f.chunk.css"
+  },
+  {
+    "revision": "f80b842d58d2324db030816002c90ca6",
+    "url": "/profile-manager/index.html"
+  }
+];
